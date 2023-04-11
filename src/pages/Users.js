fc60186f0ee706a39bf080b9992c7ed5ce1d6b99
Huyentran226112 <huyentran221100@gmail.com>
2023-04-11 15:04:23 +0700

    feature-users

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,11 +1,56 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import UserModel from "../models/UserModel";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function Users(props) {
-    return (
-        <div>
-          <h1> Users</h1>
-        </div>
-    );
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    UserModel.getAll()
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+  const handleDelete = (id) => {
+    UserModel.delete(id)
+      .then((res) => {
+        // navigate("/");
+		window.location.reload();
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("da co loi xay ra ");
+      });
+  };
+  return (
+    <div>
+      <table border={1} width={"100%"}>
+        <thead>
+          <tr>
+            <th> ID </th>
+            <th> Name </th>
+            <th> Action </th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, key) => (
+            <tr key={key}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>
+                <Link to={"/users/" + user.id}>Xem</Link>|
+                <Link to={"/users/" + user.id + "/edit"}>Sua</Link>|
+                <button id="button2" onClick={ () => handleDelete(user.id)}>delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Users;
